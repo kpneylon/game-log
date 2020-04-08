f0ce51b11db2c1629b378dd0a4c7ae46067951b6
kpneylon <kenneth_neylon@hotmail.com>
2020-04-08 08:39:20 -0700

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,71 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get "/signup" do
+		erb :signup
+  end
+  
+  post "/users" do
+		user = User.new(:username => params[:username],
+		 :password => params[:password])
+
+		if user.save
+		  erb :success
+		else
+		  redirect "/failure"
+		end
+  end
+  
+  get "/login" do
+		erb :login
+	end
+
+	post "/login" do
+		user = User.find_by(:username => params[:username])
+	   
+		if user && user.authenticate(params[:password])
+		  session[:id] = user.id
+		  erb :success
+		else
+		  redirect "/failure"
+    end
+  end
+  
+  get "/success" do
+		if logged_in?
+			erb :success
+		else
+			redirect "/login"
+		end
+	end
+
+	get "/failure" do
+		erb :failure
+	end
+
+	get "/logout" do
+		session.clear
+		redirect "/"
+	end
+
+	helpers do
+		def logged_in?
+			!!session[:id]
+		end
+
+		def current_user
+			User.find(session[:id])
+		end
+	end
+
+end
